3f44065a91422ac176fcdf50f28ce4b0c2ad49d9
garogarabed12 <garogarabed@hotmail.com>
2020-12-05 17:14:54 +0100

    Day 5.1

---
+++ src/main/java/aoc2020/day5/Day5.java
@@ -0,0 +1,25 @@
+package aoc2020.day5;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day5 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Part 1:");
+
+        Path path = Paths.get("./src/main/java/aoc2020/day5/dayFiveInput.txt");
+        SeatIdGenerator seatIdGenerator = new SeatIdGenerator();
+
+        List<Integer> seatIds = Files.lines(path).map(seatIdGenerator::generateSeatId).sorted().collect(Collectors.toList());
+        int size = seatIds.size();
+
+        System.out.println("Highest seat ID on a boarding pass is: " + seatIds.get(size-1));
+
+        System.out.println("==================================================");
+        System.out.println("Part 2:");
+    }
+}
---
+++ src/main/java/aoc2020/day5/SeatIdGenerator.java
@@ -0,0 +1,39 @@
+package aoc2020.day5;
+
+public class SeatIdGenerator {
+
+    public int generateSeatId(String ticketCode) {
+        int rowIndex = 64;
+        int seatIndex = 4;
+        int front = 0, back = 127;
+        int left = 0, right = 7;
+
+        for (int i = 0; i < ticketCode.length(); i++) {
+            switch (ticketCode.charAt(i)) {
+                case 'F':
+                    back = back - rowIndex;
+                    rowIndex = rowIndex / 2;
+                    break;
+
+                case 'B':
+                    front = front + rowIndex;
+                    rowIndex = rowIndex / 2;
+                    break;
+
+                case 'R':
+                    left = left + seatIndex;
+                    seatIndex = seatIndex / 2;
+                    break;
+
+                case 'L':
+                    right = right - seatIndex;
+                    seatIndex = seatIndex / 2;
+                    break;
+            }
+        }
+
+        /*these two are the same.
+        back * 8 + right;*/
+        return front * 8 + left;
+    }
+}
