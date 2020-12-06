7e9063723b29790b9f40aa0fd164003b3b82974f
garogarabed12 <garogarabed@hotmail.com>
2020-12-06 21:14:05 +0100

    Day 6

---
+++ src/main/java/aoc2020/day6/Day6.java
@@ -0,0 +1,30 @@
+package aoc2020.day6;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day6 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Part 1:");
+        Path path = Paths.get("./src/main/java/aoc2020/day6/daySixInput.txt");
+        List<String> lines = Files.lines(path).collect(Collectors.toList());
+
+        QuestionsCalculator questionsCalculator = new QuestionsCalculator();
+        questionsCalculator.calculateV1(lines);
+
+        System.out.print("The sum of the number of questions to which anyone answered \"yes\" is: ");
+        System.out.println(questionsCalculator.getYesAnswersCounter());
+
+        System.out.println("================================");
+        System.out.println("Part 2");
+
+        questionsCalculator.calculateV2(lines);
+
+        System.out.print("The sum of the number of questions to which everyone answered \"yes\" is: ");
+        System.out.println(questionsCalculator.getYesAnswersCounter());
+    }
+}
---
+++ src/main/java/aoc2020/day6/QuestionsCalculator.java
@@ -0,0 +1,90 @@
+package aoc2020.day6;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class QuestionsCalculator {
+
+    private final List<String> data;
+    private long yesAnswersCounter = 0;
+
+    public QuestionsCalculator() {
+        data = new ArrayList<>();
+    }
+
+    public void countTheNumberOfWithYesAnsweredQuestionsV1() {
+        List<Character> answeredQuestions = new ArrayList<>();
+        int numberOfWithYesAnsweredQuestionsPerGroup = 0;
+
+        for (String line : data) {
+            for (int i = 0; i < line.length(); i++) {
+                if (!answeredQuestions.contains(line.charAt(i))) {
+                    answeredQuestions.add(line.charAt(i));
+                    numberOfWithYesAnsweredQuestionsPerGroup++;
+                }
+            }
+        }
+
+        yesAnswersCounter += numberOfWithYesAnsweredQuestionsPerGroup;
+        data.clear();
+    }
+
+    public void countTheNumberOfWithYesAnsweredQuestionsV2() {
+        int numberOfPeopleWhoAnswered = data.size();
+
+        List<Character> answeredQuestions = new ArrayList<>();
+        int numberOfWithYesAnsweredQuestionsPerGroup = 0;
+
+        for (String line : data) {
+            for (int i = 0; i < line.length(); i++) {
+                answeredQuestions.add(line.charAt(i));
+            }
+        }
+
+        List<Character> distinctAnswers = answeredQuestions.stream().distinct().collect(Collectors.toList());
+        Map<Character, Long> frequenciesMap = answeredQuestions.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        for (Character distinctAnswer : distinctAnswers) {
+            if (frequenciesMap.get(distinctAnswer) == numberOfPeopleWhoAnswered) {
+                numberOfWithYesAnsweredQuestionsPerGroup++;
+            }
+        }
+
+        yesAnswersCounter += numberOfWithYesAnsweredQuestionsPerGroup;
+        data.clear();
+    }
+
+    public void addData(String line) {
+        data.add(line);
+    }
+
+    public void calculateV1(List<String> lines) {
+        yesAnswersCounter = 0;
+        for (String line : lines) {
+            if (!line.equals("")) {
+                addData(line);
+            } else countTheNumberOfWithYesAnsweredQuestionsV1();
+        }
+        //One last time to consider the last group.
+        countTheNumberOfWithYesAnsweredQuestionsV1();
+    }
+
+    public void calculateV2(List<String> lines) {
+        yesAnswersCounter = 0;
+        for (String line : lines) {
+            if (!line.equals("")) {
+                addData(line);
+            } else countTheNumberOfWithYesAnsweredQuestionsV2();
+        }
+        //One last time to consider the last group.
+        countTheNumberOfWithYesAnsweredQuestionsV2();
+    }
+
+    public long getYesAnswersCounter() {
+        return yesAnswersCounter;
+    }
+}
