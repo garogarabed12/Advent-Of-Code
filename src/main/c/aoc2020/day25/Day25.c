fc6c23005bb50576b97aa7ba8b798c65a2fe9922
garogarabed12 <garogarabed@hotmail.com>
2020-12-25 13:12:17 +0100

    Add Day25 Part 1

---
+++ src/main/c/aoc2020/day25/Day25.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long calculateLoopsize(long publicKey);
+
+long makeHandshake(long loopSize, long subjectNumber);
+
+int main() {
+    FILE *input = fopen("DayTwentyFiveInput.txt", "r");
+    if (!input) {
+        fclose(input);
+        return 1;
+    }
+
+    long doorPublicKey;
+    long cardPublicKey;
+    fscanf(input, "%ld", &doorPublicKey);
+    fscanf(input, "%ld", &cardPublicKey);
+
+    long loopSizeOfDoor = calculateLoopsize(doorPublicKey);
+    long loopSizeOfCard = calculateLoopsize(cardPublicKey);
+
+    long doorEncryptionKey = makeHandshake(loopSizeOfCard, doorPublicKey);
+    long cardEncryptionKey = makeHandshake(loopSizeOfDoor, cardPublicKey);
+
+    //Both should be equal
+    printf("Encryption keys are:\n%ld\n%ld\n", doorEncryptionKey, cardEncryptionKey);
+
+    fclose(input);
+    return 0;
+}
+
+long calculateLoopsize(long publicKey) {
+    long subjectNumber = 7;
+    long result = 1;
+    long loopSize = 0;
+    while(result != publicKey) {
+        result *= subjectNumber;
+        result = result % 20201227;
+        loopSize++;
+    }
+    return loopSize;
+}
+
+long makeHandshake(long loopSize, long subjectNumber) {
+    int i = 0;
+    long result = 1;
+    while (i < loopSize) {
+        result = result * subjectNumber;
+        result = result % 20201227;
+        i++;
+    }
+    return result;
+}
