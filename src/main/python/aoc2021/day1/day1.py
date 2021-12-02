960d754db23472099326bc91ca1aead17c4f1c44
garogarbed12 <garogarabed@hotmail.com>
2021-12-02 01:17:46 +0100

    Refactor

--- src/main/python/aoc2021/day1/day1.py
+++ src/main/python/aoc2021/day1/day1.py
@@ -7,14 +7,10 @@ input_file.close()
 counter = 0
 
 for i in range(1, len(data)):
-    previous = int(data[i - 1])
-    current = int(data[i])
-    if current > previous:
+    if int(data[i]) > int(data[i - 1]):
         counter = counter + 1
 
-print(counter)
-
-counter = 0
+print("Answer for part 1: %2d" % counter)
 
 
 # Part 2
@@ -23,10 +19,9 @@ def count_three_measurement_window(index):
     return sum(int(data[index]) for index in (index, index + 2))
 
 
+counter = 0
 for i in range(0, len(data) - 2):
-    previous_window_sum = count_three_measurement_window(i - 1)
-    current_window_sum = count_three_measurement_window(i)
-    if current_window_sum > previous_window_sum:
+    if count_three_measurement_window(i) > count_three_measurement_window(i - 1):
         counter = counter + 1
 
-print(counter)
+print("Answer for part 2: %2d" % counter)
